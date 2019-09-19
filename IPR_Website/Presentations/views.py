0f6181ffe6093099d9066631938c818bcff60870
Aman Bhatnagar <bhatnagar.aman1998@gmail.com>
2019-09-19 23:35:11 +0530

    Navlinks

--- IPR_Website/Presentations/views.py
+++ IPR_Website/Presentations/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def presentations(request):
+    context = {
+        "presentations": "active",
+    }
+    return render(request, 'Presentations/presentations.html', context)
--- IPR_Website/Skits/views.py
+++ IPR_Website/Skits/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def skits(request):
+    context = {
+        "skits": "active",
+    }
+    return render(request, 'Skits/skits.html', context)
