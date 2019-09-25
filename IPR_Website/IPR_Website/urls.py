cecc1958f460236e290fafb860a4db17e6aa8637
Aman Bhatnagar <bhatnagar.aman1998@gmail.com>
2019-09-25 18:07:56 +0530

    Images Fixed

--- IPR_Website/IPR_Website/urls.py
+++ IPR_Website/IPR_Website/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='website/logout.html'), name='logout'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
